42a45a6f979a61c919412614541dc21b0c33585b
cocociti <hello@cocociti.com>
2015-12-23 18:14:36 +0530

    syntax issue in Global Variable manager email id

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,5 +1,5 @@
 class AdminMailer < ApplicationMailer
-  default bcc: "#{GLOBAL_VARIABLES[:]manager_email_id_1}"
+  default bcc: "#{GLOBAL_VARIABLES[:manager_email_id_1]}"
   def new_user(user)
     @user = user
     mail to: GLOBAL_VARIABLES[:notification_mail], subject: "New User: #{user.email} has joined cocociti"

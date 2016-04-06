461384fe5d83dc8d80e3313be0a6812d49d32d16
cocociti <hello@cocociti.com>
2016-04-07 01:34:24 +0530

    tag cloud implemented

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,9 @@
 class Tag < ActiveRecord::Base
   has_many :taggings
   has_many :showcases, through: :taggings
+
+  def self.counts
+    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id, tags.name").order(count: :desc).limit(10)
+  end
+
 end

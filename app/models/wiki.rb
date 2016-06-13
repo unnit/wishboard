a60500055901cd4fc539ca3091a2e0352ff5f45d
cocociti <hello@cocociti.com>
2016-06-13 16:08:37 +0530

    wiki desc character limit increased to 6000, meta content updated if no bio present

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -3,5 +3,5 @@ class Wiki < ActiveRecord::Base
 
   validates :title, :description, presence: true
   validates :title, length: { maximum: 200 }
-  validates :description, length: { maximum: 2000 }
+  validates :description, length: { maximum: 6000 }
 end

519b49e72dfd586c430743da4600424214c8e708
Dheeraj <t@cocociti.com>
2017-07-27 02:19:43 +0530

    first chatroom meesage delay fixed;window on load function used as channel was getting setup after page load

--- app/jobs/chat_broadcast_job.rb
+++ app/jobs/chat_broadcast_job.rb
@@ -3,7 +3,7 @@ class ChatBroadcastJob < ApplicationJob
 
   def perform(message)
    ActionCable.server.broadcast "chat_room_#{message.chat_room_id}", message: render_message(message), owner_id: message.user.id, chat_id: message.id, chat_room_id: message.chat_room_id
-  	message.chat_room.memberships.where(online: true).each do |membership|
+  	message.chat_room.memberships.each do |membership|
   		ActionCable.server.broadcast "user_#{membership.user.id}", chat_message_count: membership.user.unread_chat_messages_count, chat_room_id: message.chat_room.id
   	end
   end

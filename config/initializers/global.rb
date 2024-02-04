ff9af93455045dd030a487fe5072dd3633fd5a83
Dheeraj T <dheerajt@liaisonedu.com>
2024-02-04 10:20:18 +0530

    global file updated

--- config/initializers/global.rb
+++ config/initializers/global.rb
@@ -1,4 +1,4 @@
 Global.configure do |config|
-  config.environment = Rails.env.to_s
+  config.environment = Rails.env.to_s || 'production'
   config.config_directory = Rails.root.join('config/global').to_s
 end

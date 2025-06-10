594eb51509f215887c7187666fef9217c8f20c76
Dheeraj <unnithanth@gmail.com>
2025-06-10 01:22:25 -0400

    appconfig fixes

--- config/initializers/appconfig.rb
+++ config/initializers/appconfig.rb
@@ -1,5 +1,5 @@
-FACEBOOK_CONFIG = YAML.load_file("#{Rails.root}/config/facebook.yml")[Rails.env].symbolize_keys
-CITRUS_CONFIG = YAML.load_file("#{Rails.root}/config/citrus.yml")[Rails.env].symbolize_keys
-GLOBAL_VARIABLES = YAML.load_file("#{Rails.root}/config/global_variables.yml")[Rails.env].symbolize_keys
-PLIVO_CONFIG = YAML.load_file("#{Rails.root}/config/plivo.yml")[Rails.env].symbolize_keys
-FIREBASE_CONFIG= YAML.load_file("#{Rails.root}/config/firebase.yml")[Rails.env].symbolize_keys
+FACEBOOK_CONFIG = YAML.load_file("#{Rails.root}/config/facebook.yml")[Rails.env]&.symbolize_keys || {}
+CITRUS_CONFIG = YAML.load_file("#{Rails.root}/config/citrus.yml")[Rails.env]&.symbolize_keys || {}
+GLOBAL_VARIABLES = YAML.load_file("#{Rails.root}/config/global_variables.yml")[Rails.env]&.symbolize_keys || {}
+PLIVO_CONFIG = YAML.load_file("#{Rails.root}/config/plivo.yml")[Rails.env]&.symbolize_keys || {}
+FIREBASE_CONFIG= YAML.load_file("#{Rails.root}/config/firebase.yml")[Rails.env]&.symbolize_keys || {}
